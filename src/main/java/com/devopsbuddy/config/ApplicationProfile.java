511a900fed6c1f4fa61c06b888e902150761078f
SabiKov <skovacs33@gmail.com>
2017-08-06 20:37:36 +0200

    Externalised webmaster login and email details

--- src/main/java/com/devopsbuddy/config/ApplicationProfile.java
+++ src/main/java/com/devopsbuddy/config/ApplicationProfile.java
@@ -1,11 +1,13 @@
 package com.devopsbuddy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.devopsbuddy.backend.persistence.repositories")
 @EnableTransactionManagement
+@PropertySource("file:\\Users\\Sabi\\DropBox\\Accounts_info\\Stripe\\application-common.properties")
 public class ApplicationProfile {
 }
